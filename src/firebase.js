ba302ad19d1cdef99da78fd9c507b63d7cdfce16
Troycreator <135917896+Troycreator@users.noreply.github.com>
2025-07-10 03:08:34 +0200

    Update firebase.js
    remoing sensitive information

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC-BIiv62hMyo5Duj8k7wJT8IMRJgoEBXM",
-  authDomain: "backend-f4dbc.firebaseapp.com",
-  projectId: "backend-f4dbc",
-  storageBucket: "backend-f4dbc.firebasestorage.app",
-  messagingSenderId: "767897493750",
-  appId: "1:767897493750:web:9637b98b9249b1ccbe8cd7",
-  measurementId: "G-FHYDQ2TSD2"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
